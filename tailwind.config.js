df335e2390c2f0fb6508e58847b3677ac330eaa7
AilinMari <ailin_scilla@hotmail.com>
2025-08-21 14:33:51 +0200

    Making log in form

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      boxShadow: {
+        'gold-glow': '0 4px 20px 0 rgba(212, 175, 55, 0.5)', // custom gold shadow
+        // Add more custom shadows here
+      },
+    },
+  },
+  plugins: [],
+};
