4e685118748530424ab648030b4f28d37c2011db
AilinMari <ailin_scilla@hotmail.com>
2025-08-21 10:56:20 +0200

    started with the design, navbar

---
+++ src/js/api/constants.js
@@ -0,0 +1,13 @@
+export const API_BASE_URL = "https://v2.api.noroff.dev";
+
+export const API_AUTH = `${API_BASE_URL}/auth`;
+
+export const API_AUTH_LOGIN = `${API_BASE_URL}/auth/login`;
+
+export const API_AUTH_REGISTER = `${API_BASE_URL}/auth/register`;
+
+export const API_LISTINGS = `${API_BASE_URL}/auction-house/listings`;
+
+export const API_PROFILES = `${API_BASE_URL}/auction-house/profiles`;
+
+export const API_AUTH_KEY = `${API_AUTH}/create-api-key`;
