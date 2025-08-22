3799d8e948c2e4223978e3b7774a5ffa302b8b9d
AilinMari <ailin_scilla@hotmail.com>
2025-08-22 13:12:29 +0200

    login, registert and home design basics

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'gold-glow': '0 4px 20px 0 rgba(212, 175, 55, 0.5)', // custom gold shadow
+        "gold-glow": "0 4px 20px 0 rgba(212, 175, 55, 0.5)", // custom gold shadow
         // Add more custom shadows here
       },
     },
