3a3dcb9aba490000de68559d6555730a9f25b5fc
AilinMari <ailin_scilla@hotmail.com>
2025-08-25 12:29:45 +0200

    logout working

---
+++ src/js/api/logout.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const logoutButton = document.querySelector("#logout");
+
+  if (logoutButton) {
+    logoutButton.addEventListener("click", () => {
+      try {
+        // Clear user data from local storage
+        localStorage.removeItem("token");
+        localStorage.removeItem("name");
+        localStorage.removeItem("accessToken");
+
+        localStorage.removeItem("apiKey");
+        localStorage.removeItem("avatar");
+
+        // Redirect to the login page
+        window.location.href = "/auth/login/";
+      } catch (error) {
+        console.error("Error during logout:", error.message);
+      }
+    });
+  }
+});
