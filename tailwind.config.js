cccf0271635a82309411dbc67bdebe5b59fc200f
AilinMari <ailin_scilla@hotmail.com>
2025-09-12 19:31:02 +0200

    trying to fetch the fonts again for the deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+// tailwind.config.js
 module.exports = {
   content: [
     "./public/**/*.html",
     "./public/**/*.js",
-    "./src/**/*.{html,js,jsx,ts,tsx}"
+    "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', "serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
